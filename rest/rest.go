fc9a46dcb16bb4c117db47ccd4dea69bbd69048c
sdavidson15 <davidson.sc19@gmail.com>
2018-10-21 19:09:53 -0500

    Redirect http to https

--- rest/rest.go
+++ rest/rest.go
@@ -1,14 +1,18 @@
 package rest
 
 import (
+	"log"
 	"net/http"
+	"strings"
+	"wishlist/common"
 
 	"github.com/gorilla/mux"
-
-	"wishlist/common"
 )
 
 func Setup(manager *common.Manager, restUri string) *mux.Router {
+	// Redirect http to https
+	go http.ListenAndServe(":80", http.HandlerFunc(redirect))
+
 	router := mux.NewRouter().StrictSlash(true)
 	h := &Handler{manager}
 	for _, route := range h.Routes() {
@@ -25,4 +29,12 @@ func Setup(manager *common.Manager, restUri string) *mux.Router {
 	}
 
 	return router
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	host := strings.Split(r.Host, ":")[0]
+	target := "https://" + host + r.URL.Path
+	// Concatenate query params here, if necessary
+	log.Printf("Redirect to %s", target)
+	http.Redirect(w, r, target, http.StatusTemporaryRedirect)
 }
